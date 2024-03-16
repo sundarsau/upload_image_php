7b06700a0c4d4bad0d0cd828c5ca9a596d06c6e1
sundarsau <sundarsau@gmail.com>
2024-03-16 17:47:33 +0530

    database table

---
+++ users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
